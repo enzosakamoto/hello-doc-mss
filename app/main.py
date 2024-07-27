8d2b3351acee9697ea369f48bbaf3ad439fac689
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-26 22:45:17 -0300

    fix: add cors policy

--- app/main.py
+++ app/main.py
@@ -8,11 +8,13 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
+    "http://3.80.203.53",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
